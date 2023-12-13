2deba999eab334b22f69d7a022a3a1997e4b50f7
vivar <bobojonoffff@gmail.com>
2023-12-13 18:54:02 +0300

    fix ind part 2 lab02

--- src/main/java/ru/bobojonov/MySecondTestAppSpringBoot/model/Request.java
+++ src/main/java/ru/bobojonov/MySecondTestAppSpringBoot/model/Request.java
@@ -1,6 +1,9 @@
 package ru.bobojonov.MySecondTestAppSpringBoot.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +15,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Request {
     @NotBlank
+    @Size(max = 32)
     private String uid;
+    @NotBlank
+    @Size(max = 32)
     private String operationUid;
     private String systemName;
+    @NotBlank
     private String systemTime;
     private String source;
+    @Min(1)
+    @Max(100000)
     private int communicationId;
     private int templateId;
     private int productCode;
